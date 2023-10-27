20eac729f30eb2bee18ece60a8f0459c6ac5c9cb
emilecode <emile.hiot@proton.me>
2023-10-27 16:46:56 +0200

    add script typed.js

---
+++ js/typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: ["Codeur en Herbe", "Passionné", "Autodidacte", "Rêveur"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  startDelay: 1000,
+  backDelay: 1500,
+  showCursor: false,
+  loop: true,
+  shuffle: true,
+  smartBackspace: true,
+});
